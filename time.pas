2b9d496be23dfd8f19b676a66acc27de8017fc29
Павел Соляных <pasha.sol788@yandex.ru>
2023-09-30 10:49:48 +0300

    Add new file

--- time.pas
+++ time.pas
@@ -3,5 +3,5 @@
   if (n mod 100 =0) and (n mod 400 <>0) then
     print('Не високосный , 365 дней')
   else if n mod 4 =0 then
-    print('Високосный , 366 дней')
+    print('Високосный , 366 дней')//test
 end.
---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var h:=readinteger('Введите кол-во часов');
+  var m:=readinteger('Введите кол-во минут');
+  var s:=readinteger('Введите кол-во секунд');
+  var res := h*3600+m*60 +s;
+  print(res);
+end.
