ceb52511202abefe0ea99d62b67720f6a5c53142
Павел Соляных <pasha.sol788@yandex.ru>
2023-09-30 10:40:22 +0300

    Add new file

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var n:=readinteger('Введите год : ');
+  if (n mod 100 =0) and (n mod 400 <>0) then
+    print('Не високосный')
+  else if n mod 4 =0 then
+    print('Високосный')
+  else
+    print('Не високосный')
+end.
