e22dbe46eadc7068eda0d3d8753c499031142e3e
Павел Соляных <pasha.sol788@yandex.ru>
2023-09-30 10:44:43 +0300

    Add days

--- time.pas
+++ time.pas
@@ -1,9 +1,7 @@
 ﻿begin
   var n:=readinteger('Введите год : ');
   if (n mod 100 =0) and (n mod 400 <>0) then
-    print('Не високосный')
+    print('Не високосный , 365 дней')
   else if n mod 4 =0 then
-    print('Високосный')
-  else
-    print('Не високосный')
+    print('Високосный , 366 дней')
 end.
